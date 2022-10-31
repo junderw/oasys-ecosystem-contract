41e21f2c49225cde087570314347c30045529fd2
ironbeer <7997273+ironbeer@users.noreply.github.com>
2022-10-31 20:16:59 +0900

    Added cBridge contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+    ],
+  },
 };
 
 export default config;
